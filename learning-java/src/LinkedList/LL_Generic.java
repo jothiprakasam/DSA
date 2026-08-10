7117f7d17bef2d34b25c1f8b28872ae9efb42aed
jothiprakasam <mrameshsai74@gmail.com>
2026-08-10 18:53:08 +0530

    added iterator for custom LL.

--- learning-java/src/LinkedList/LL_Generic.java
+++ learning-java/src/LinkedList/LL_Generic.java
@@ -1,20 +1,25 @@
 package LinkedList;
-
+import java.lang.Iterable;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+//Driver code
 public class LL_Generic{
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LL<Integer> ll = new LL<>();
 		ll.insert(2);
 		ll.insert(9);
-		System.out.println(ll.getHead());
+		//Iterator<Integer> it = ll.iterator();
+		for(int num: ll) {
+			System.out.println(num);
+		}
 	}
-
 }
 
-class LL<T>{
-	class Node<T>{
+//Linked list custom collection
+class LL<T> implements Iterable<T>{
+	static class Node<T>{
 		T data;
 		Node<T> next;
 		Node(T data){
@@ -41,4 +46,30 @@ class LL<T>{
 			curr.next=newNode;
 		}
 	}
+	// iterator
+	@Override 
+	public Iterator<T> iterator(){
+		return new LinkedListIterator<T>(head);
+	}
+	
 }
+
+//iterator for the linked list 
+class LinkedListIterator<T> implements Iterator<T>{
+	LL.Node<T> current;
+	LinkedListIterator(LL.Node<T> head){
+		this.current=head;
+	}
+	@Override
+	public boolean hasNext() {
+	return current!=null;
+	}
+	@Override
+	public T next() {
+		if(!hasNext()) throw new NoSuchElementException();
+		T data = current.data;
+		current = current.next;
+		return data;
+	}
+}
+
